a4f1abbba7213519bbee3537f84aed1395803f3e
Keziah Biermann <development@kizzycode.de>
2022-08-30 00:13:07 +0200

    Remove initial build step (use a persistent site directory instead)

--- files/start.sh
+++ files/start.sh
@@ -61,18 +61,7 @@ function setup_repository() {
 }
 
 
-# Perform a first build if the repo is populated
-function first_build() {
-    # Check if the repo has any commits
-    if git ls-remote --exit-code "$HOMEDIR/git" >/dev/null; then
-        # Build site
-        su jekyll -s "/bin/sh" -c "/libexec/jekyll-post-receive.sh" || echo "!> Warning: Failed to rebuild site"
-    fi
-}
-
-
 # Prepare and start server
 setup_ssh
 setup_repository
-first_build
 exec supervisord -c "/etc/supervisord.conf"
